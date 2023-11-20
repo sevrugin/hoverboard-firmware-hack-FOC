bd40f0a529fb83035ccb2d11b44667209b8d66eb
Sergey <sevrugin@gmail.com>
2023-11-20 18:47:18 +0200

    Cells count

--- Src/bldc.c
+++ Src/bldc.c
@@ -79,8 +79,8 @@ static int16_t offsetdcl    = 2000;
 static int16_t offsetdcr    = 2000;
 
 uint8_t        BAT_CELLS        = 0;
-int16_t        batVoltage       = (400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE;
-static int32_t batVoltageFixdt  = (400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE << 16;  // Fixed-point filter output initialized at 400 V*100/cell = 4 V/cell converted to fixed-point
+int16_t        batVoltage       = 0;//(400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE;
+static int32_t batVoltageFixdt  = 0;//(400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE << 16;  // Fixed-point filter output initialized at 400 V*100/cell = 4 V/cell converted to fixed-point
 
 // =================================
 // DMA interrupt frequency =~ 16 kHz
